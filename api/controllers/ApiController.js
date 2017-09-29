1a7b59288a1d69e3a11f0fddcb67ab1d31ce1d1b
David <f165nibiru@gmail.com>
2017-09-29 17:47:39 +0200

    report

--- api/controllers/ApiController.js
+++ api/controllers/ApiController.js
@@ -18,7 +18,7 @@ const ApiController = module.exports = {
   /**
    * @param req
    * @param res
-   * @req_params {"tid":number, "reply":number<id of student reply>, "status":number<0:ok,1:note,2:warning,3:error>, "passed": boolean<false:repost task to student,true: tests passed>, "raport":string<url>, "message":string<short raport message>, "rk":string<request key>}
+   * @req_params {"tid":number, "reply":number<id of student reply>, "status":number<0:ok,1:note,2:warning,3:error>, "passed": boolean<false:repost task to student,true: tests passed>, "report":string<url>, "message":string<short report message>, "rk":string<request key>}
    */
   changeMachineStatus: function (req, res) {
     if(!req.is('json')){
@@ -26,18 +26,18 @@ const ApiController = module.exports = {
     }
     let data = req.body
     if (!_.isInteger(data.tid) || !_.isInteger(data.reply) || !_.isInteger(data.status) || !_.isBoolean(data.passed)
-      || !_.isString(data.raport) || !_.isString(data.message) || !_.isString(data.rk)) {
+      || !_.isString(data.report) || !_.isString(data.message) || !_.isString(data.rk)) {
       return res.badRequest({'error':'E_PARAM_EMPTY'})
     }
     if(data.status<0 || data.status> 3){
       return res.badRequest({'error':'E_INVALID_STATUS'})
     }
 
-    let generated_rk = md5(data.tid.toString() + data.reply.toString() + data.status.toString() + data.passed.toString() + data.raport + data.message + ManageReplies.machine.apiKey)
+    let generated_rk = md5(data.tid.toString() + data.reply.toString() + data.status.toString() + data.passed.toString() + data.report + data.message + ManageReplies.machine.apiKey)
     if(data.rk !== generated_rk){
       return res.badRequest({'error':'E_INVALID_REQUEST_KEY'})
     }
-    ManageReplies.machine.changeStatus(data.tid,data.reply,data.status,data.passed,data.raport,data.message,(err)=>{
+    ManageReplies.machine.changeStatus(data.tid,data.reply,data.status,data.passed,data.report,data.message,(err)=>{
       if(err){
         switch (err.code){
           case 'E_REPLY_NOT_FOUND':
